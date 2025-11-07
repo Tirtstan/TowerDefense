5c289f39bfaaa78f4d4d0b13e9ab5a90f380d489
Tirtstan <tristan.gold2004@outlook.com>
2025-11-07 23:27:17 +0200

    title font + upgrade content

--- Assets/Scripts/Towers/CenterTower.cs
+++ Assets/Scripts/Towers/CenterTower.cs
@@ -1,16 +1,6 @@
-using System;
 using UnityEngine;
 
 public class CenterTower : Singleton<CenterTower>
 {
-    [Header("Health")]
-    [SerializeField]
-    private TowerHealth towerHealth;
-
-    protected override void Awake()
-    {
-        base.Awake();
-    }
-
     public Vector3 GetPosition() => transform.position;
 }
